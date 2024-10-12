980432ae46b0dab6bbd78300bd24956914cbbd09
Jason Scott <jcpoems@gmail.com>
2024-10-12 10:44:32 -0400

    set up skeleton more
    made webpack hot reload html, added rough classes for project and class

--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
 import "./styles.css";
-console.log("hi Jason :3");
+import {changeText} from "./domLogic.js";
+
+console.log("working");
+
+class Project {
+    constructor(name){
+        this.name = name;
+        this.todos = [];
+    }
+
+}
+
+class Todo {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.checked = false;
+        this.priority = priority;
+        this.notes = "";
+    }
+}
+
+const projArray = [];
+
+
+
+
+
