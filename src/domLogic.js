2c64fbf676d8f8e68f4b9260099f6da68be59cc2
Jason Scott <jcpoems@gmail.com>
2024-12-05 14:23:42 -0500

    Got sidetracked, restarting this project. Keep moving forward.

--- src/domLogic.js
+++ src/domLogic.js
@@ -1,14 +1,2 @@
 
-const body = document.querySelector("body");
-
-function changeText(text){
-    body.textContent = text;
-}
-
-console.log("bruh");
-//body.textContent = "chill";
-
-
- 
-export {changeText };
 
--- src/main.js
+++ src/main.js
@@ -1,28 +1,7 @@
 import "./styles.css";
 import {changeText} from "./domLogic.js";
 
-console.log("working");
 
-class Project {
-    constructor(name){
-        this.name = name;
-        this.todos = [];
-    }
-
-}
-
-class Todo {
-    constructor(title, description, dueDate, priority){
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.checked = false;
-        this.priority = priority;
-        this.notes = "";
-    }
-}
-
-const projArray = [];
 
 
 
